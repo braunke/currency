45d98cdc13e71efa4b2744979f620d482f23269b
braunke <go1018bx@go.minneapolis.edu>
2017-03-07 23:14:36 -0600

    connected to currency site

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
+var currencylayer = require('../helpers/currencylayer');
 var router = express.Router();
-
-var exchangeRates = { 'EUR' : 0.94, 'JPY' : 112.86, "USD" : 1.0 };
 
 /* Handle GET request for home page */
 router.get('/', function(req, res){
@@ -9,23 +8,30 @@ router.get('/', function(req, res){
     res.render('index');
 });
 
-
 /* Handle currency form submit */
 router.get('/convert', function(req, res){
-    var originalCurrencyAmount = parseFloat(req.query.dollar_amount);
+
     //gets the original currency
     var originalCurrency = req.query.fromCurrency;
-    //changes to dollars
-    var originalToDollarRate = exchangeRates[originalCurrency];
-    var dollars = originalCurrencyAmount / originalToDollarRate;
     //gets the currency you are changing to
     var desiredCurrency = req.query.to_currency;
-    var dollarToDesiredRate = exchangeRates[desiredCurrency];
-    //does the conversion
-    var result = dollars * dollarToDesiredRate;
-    //results, money about formatted to look better
-    res.render('results', { dollars : originalCurrencyAmount.toFixed(2),
-        result: result.toFixed(2), currency: desiredCurrency, from : originalCurrency})
+    currencylayer(originalCurrency, desiredCurrency, function (data, error) {
+        if (error) {
+            console.log(error);
+        } else {
+            var originalCurrencyAmount = parseFloat(req.query.dollar_amount);
+            //changes to dollars
+            var originalToDollarRate = data.originalToDollarRate;
+            var dollars = originalCurrencyAmount / originalToDollarRate;
+            var dollarToDesiredRate = data.dollarToDesiredRate;
+            //does the conversion
+            var result = dollars * dollarToDesiredRate;
+        }
+        //results, money about formatted to look better
+        res.render('results', { dollars : originalCurrencyAmount.toFixed(2),
+            result: result.toFixed(2), currency: desiredCurrency, from : originalCurrency})
+    });
 });
+
 
 module.exports = router;
